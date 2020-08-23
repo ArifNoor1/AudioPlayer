989a69aa263d90d5126c7b86090174caace129d4
Mohammad Arif <arif.techunicorns@gmail.com>
2020-08-23 15:50:46 +0530

    Toast on every buttons are implemented

--- app/src/main/java/com/example/audioplayer/MainActivity.java
+++ app/src/main/java/com/example/audioplayer/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +18,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mp = MediaPlayer.create(this,R.raw.abhi);
+        Toast.makeText(this, "MP3 is on", Toast.LENGTH_SHORT).show();
     }
 
     public void startSong(View view){
         mp.start();
+        Toast.makeText(this, "MP3 is pause", Toast.LENGTH_SHORT).show();
     }
 
     public void pauseSong(View view){
@@ -29,5 +32,6 @@ public class MainActivity extends AppCompatActivity {
     public void stopSong(View view){
         mp.stop();
         mp = MediaPlayer.create(this,R.raw.abhi);
+        Toast.makeText(this, "MP3 is stop", Toast.LENGTH_SHORT).show();
     }
 }
